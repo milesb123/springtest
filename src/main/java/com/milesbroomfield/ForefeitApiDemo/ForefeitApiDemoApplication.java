5b5431b0883c5307c6402eb59a885ce4eb7ae2b3
milesbroomfield <milesbroomfield@yahoo.com>
2022-01-27 00:16:42 +0000

    first commit

---
+++ src/main/java/com/milesbroomfield/ForefeitApiDemo/ForefeitApiDemoApplication.java
@@ -0,0 +1,13 @@
+package com.milesbroomfield.ForefeitApiDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForefeitApiDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForefeitApiDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/milesbroomfield/ForefeitApiDemo/ForefeitApiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.milesbroomfield.ForefeitApiDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForefeitApiDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
